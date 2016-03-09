aca99af19ff781dd7acd54f6ad281f1e3efc0f94
Eden <edenshapiro@gmail.com>
2016-03-09 02:45:43 -0800

    Midnight/Daytime Theme App with NSUserDefaults (butactuallythistime)

--- NSUserDefaults Example/AppDelegate.swift
+++ NSUserDefaults Example/AppDelegate.swift
@@ -12,8 +12,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+//    registerDefaults() 
+    func launchedForFirstTime(){
+        if(NSUserDefaults.standardUserDefaults().boolForKey("HasLaunchedBefore")){
+            print("App has launched before")
+        }
+        else {
+            print("This is the first launch ever!")
+            NSUserDefaults.standardUserDefaults().setBool(true, forKey: "HasLaunchedBefore")
+            NSUserDefaults.standardUserDefaults().synchronize()
+            NSUserDefaults.standardUserDefaults().setValue(false, forKey: "midnightThemeOn")
 
-
+            
+        }
+    }
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
@@ -43,4 +57,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 }
+
+
 
--- NSUserDefaults Example/ViewController.swift
+++ NSUserDefaults Example/ViewController.swift
@@ -2,24 +2,94 @@
 //  ViewController.swift
 //  NSUserDefaults Example
 //
-//  Created by Eden Shapiro on 3/1/16.
+//  Created by Eden on 3/1/16.
 //  Copyright © 2016 Udacity. All rights reserved.
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var midnightThemeLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let boolValue = NSUserDefaults.standardUserDefaults().valueForKey("midnightThemeOn"){
+            if boolValue as! Bool {
+                switchToMidnight()
+            }
+        }
+//
+//        } else {
+//            NSUserDefaults.standardUserDefaults().setValue(false, forKey: "midnightThemeOn")
+//            print("App is launching for the very first time!")
+//        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func switchFlipped(sender: AnyObject) {
+        var midnightOn: Bool
+        if themeSwitch.on {
+            switchToMidnight()
+            midnightOn = true
+        } else {
+            switchToDaytime()
+            midnightOn = false
+        }
+        NSUserDefaults.standardUserDefaults().setValue(midnightOn, forKey: "midnightThemeOn")
+       
     }
+    
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func switchToMidnight(){
+        imageView.image = UIImage(named:"moon")
+        mainView.backgroundColor = UIColor(red: 28/255, green: 19/255, blue: 76/255, alpha: 1.0)
+        midnightThemeLabel.textColor = UIColor.whiteColor()
+        titleLabel.textColor = UIColor.whiteColor()
+        //themeSwitch.tintColor = UIColor.whiteColor()
+        themeSwitch.on = true
+    }
+    
+    func switchToDaytime(){
+        imageView.image = UIImage(named:"sun")
+        mainView.backgroundColor = UIColor(red: 220/255, green: 240/255, blue: 246/255, alpha: 1.0)
+        midnightThemeLabel.textColor = UIColor.blackColor()
+        titleLabel.textColor = UIColor.blackColor()
+        //themeSwitch.tintColor = UIColor.yellowColor()
+        themeSwitch.on = false
+    }
 }
 
+
+//print("there is a value for midnightThemeOn, and it's \(boolValue)")
+//print("initial bool is true")
+// else {
+//    print("initial bool is false")
+//    
+//}
